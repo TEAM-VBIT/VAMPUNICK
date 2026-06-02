5872973e7ca37f75dd8aa8bd1a98e4eb5fdf2e00
RamRPLover <janiofficialgamer@gmail.com>
2026-06-02 19:26:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class Config:
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", "50"))
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", "99999999999"))
         self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", "20"))
-        self.YOUTUBE_API_KEY = getenv("YOUTUBE_API_KEY", "INFLEX68575028D")
+        self.YOUTUBE_API_KEY = getenv("YOUTUBE_API_KEY", "INFLEX87773028D")
         self.COOKIES_URL = [
             url for url in getenv("COOKIES_URL", "").split(" ")
             if url and "batbin.me" in url
